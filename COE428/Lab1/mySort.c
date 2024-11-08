33ee34a48cfc267c774532757e0db4036f659ce3
harshsolanki2004 <harshsolanki.2405@gmail.com>
2024-11-08 12:09:42 -0500

    Add files via upload

---
+++ COE428/Lab1/mySort.c
@@ -0,0 +1,12 @@
+void mySort(int d[], unsigned int n)
+{
+	for (int j = 1; j<n; j++){
+		int key = d[j];
+		int i = j-1;
+		while(i>=0 && d[i]>key){
+			d[i+1] = d[i];
+			i--;
+			d[i+1] = key;
+		}
+	}
+}
