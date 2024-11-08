4cf8c087d39467286471ec8829617dec99b8e8fa
harshsolanki2004 <harshsolanki.2405@gmail.com>
2024-11-08 13:21:01 -0500

    Add files via upload

---
+++ COE428/Lab2/towersMain.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "towers.h"
+
+int main(int argc, char **argv)
+{
+    int n = 3;
+    int from = 1;
+    int dest = 2;
+    if (argc > 1) {
+        n = atoi(argv[1]);
+        from = atoi(argv[2]);
+        dest = atoi(argv[3]);
+    }
+    towers(n, from, dest);
+    exit(0);
+}
+
